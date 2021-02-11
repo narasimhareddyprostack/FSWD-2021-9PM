adf2dfac2c267d98e5552ae201399c136a9df5a4
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-02-11 22:08:13 +0530

    okie

---
+++ JS/Interview/BankCustomer.js
@@ -0,0 +1,12 @@
+class BankCustomer {
+  constructor(a, b, c) {
+    this.acc_No = a;
+    this.acc_Name = b;
+    this.acc_Mobile = c;
+  }
+}
+
+let x = new BankCustomer(101, "Narasimha", 9591);
+console.log(x);
+
+//class - template to construct object
