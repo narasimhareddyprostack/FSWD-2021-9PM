8829b00272fb3275c4e389f43b6b02774f7d9dd2
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-02-13 20:54:57 +0530

    react class comp

---
+++ reactApp/reactapp/src/App copy.js
@@ -0,0 +1,20 @@
+let App = () => (
+  <div>
+    <h1>Hello , GM</h1>
+    <p> Testing</p>
+  </div>
+);
+export default App;
+
+/* function App() {
+  return <h1> Hello, GM...........</h1> <p> Testing</p>;
+}
+export default App;
+ */
+
+/*
+    Fat Arrow 
+    1. consice code
+    2. implicit return
+
+ */
--- reactApp/reactapp/src/App.js
+++ reactApp/reactapp/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+/* let App = () => {
+  return <h1>Hello, GM</h1>;
+};
+export default App;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ */
+import React from "react";
+class App extends React.Component {
+  render() {
+    return <h1>Hello, GM from React Class Component </h1>;
+  }
 }
 
 export default App;
