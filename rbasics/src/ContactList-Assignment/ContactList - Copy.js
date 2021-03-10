3f087e68e1190a8d30c146d5e0c0e28c9f6f5680
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-03-10 23:23:07 +0530

    okie

---
+++ rbasics/src/ContactList-Assignment/ContactList - Copy.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+
+class ContactList extends Component {
+  constructor(props) {
+    super(props);
+  }
+  ClickContact = (contact) => {
+    this.props.pullData(contact);
+  };
+  render() {
+    return (
+      <>
+        {/*   <pre>{JSON.stringify(this.props.contacts)}</pre> */}
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <table className="table table-hover table-primary table-striped">
+                <thead className="bg-primary text-white">
+                  <tr>
+                    <th>ID</th>
+                    <th>Image</th>
+                    <th>Name</th>
+                    <th>Age</th>
+                    <th>Location</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.props.contacts != null ? (
+                    <>
+                      {this.props.contacts.map((contact) => {
+                        return (
+                          <>
+                            <tr
+                              onMouseOver={this.ClickContact.bind(
+                                this,
+                                contact
+                              )}
+                            >
+                              <td>{contact.login.uuid}</td>
+                              <td>
+                                <img src={contact.picture.medium} />
+                              </td>
+                              <td>{contact.name.first}</td>
+                              <td>{contact.dob.age}</td>
+                              <td>{contact.location.city}</td>
+                            </tr>
+                          </>
+                        );
+                      })}
+                    </>
+                  ) : null}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default ContactList;
