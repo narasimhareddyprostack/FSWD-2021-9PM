b35622c5b71f30002f50e2a0efc7970e0f2b8a48
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-04-18 20:03:46 +0530

    okie

--- procart/client/src/modules/user/components/Login.jsx
+++ procart/client/src/modules/user/components/Login.jsx
@@ -1,8 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+
 let Login = () => {
+  let [user, setUser] = useState({ email: "", password: "" });
+  let inputHandler = (event) => {
+    /*  console.log(event);
+    console.log(event.target.name);
+    console.log(event.target.value); */
+    setUser({ ...user, [event.target.name]: event.target.value });
+  };
+  let submitHandler = (event) => {
+    console.log(user);
+    //dispatch - Redux action to consume Login API/ backend API
+    event.preventDefault();
+  };
   return (
     <React.Fragment>
-      <h1> Login Component Data</h1>
+      <section className="bg-warning p-2">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <div className="h3">
+                <p>Login Details</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className="container m-4">
+          <div className="row">
+            <div className="col-md-5 m-auto">
+              <div className="card">
+                <div className="card-header">
+                  <h4> Login Form</h4>
+                  <pre>{JSON.stringify(user)}</pre>
+                </div>
+                <div className="card-body">
+                  <form onSubmit={submitHandler}>
+                    <div className="form-group">
+                      <input
+                        type="email"
+                        placeholder="Email"
+                        className="form-control"
+                        onChange={inputHandler}
+                        name="email"
+                      />
+                    </div>
+                    <div className="form-group">
+                      <input
+                        type="password"
+                        placeholder="Password"
+                        className="form-control"
+                        onChange={inputHandler}
+                        name="password"
+                      />
+                    </div>
+                    <div className="form-group">
+                      <input
+                        type="submit"
+                        className="form-control btn btn-success"
+                      />
+                    </div>
+                  </form>
+                  <small>
+                    Don't <a href="/signup">Have Account</a>
+                  </small>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </React.Fragment>
   );
 };
--- procart/client/src/modules/user/components/Signup.jsx
+++ procart/client/src/modules/user/components/Signup.jsx
@@ -1,8 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 let Signup = () => {
+  let [user, setUser] = useState({ name: "", email: "", password: "" });
+  let inputHandler = (event) => {
+    setUser({ ...user, [event.target.name]: event.target.value });
+  };
+  let submitHandler = (event) => {
+    console.log(user);
+    event.preventDefault();
+  };
   return (
     <React.Fragment>
-      <h1> Registraion Component Data</h1>
+      <section className="bg-warning p-2">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <div className="h3">
+                <p>Registration Details Details</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="mt-4">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-5 m-auto">
+              <div className="card">
+                <div className="card-header">
+                  <h4> Signup - Form</h4>
+                  <pre>{JSON.stringify(user)}</pre>
+                </div>
+                <div className="card-body">
+                  <form onSubmit={submitHandler}>
+                    <div className="form-group">
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="User Name"
+                        name="name"
+                        onChange={inputHandler}
+                      />
+                    </div>
+                    <div className="form-group">
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Email"
+                        name="email"
+                        onChange={inputHandler}
+                      />
+                    </div>
+                    <div className="form-group">
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Password"
+                        name="password"
+                        onChange={inputHandler}
+                      />
+                    </div>
+                    <div className="form-group">
+                      <input
+                        type="submit"
+                        className="form-control btn btn-primary"
+                      />
+                    </div>
+                  </form>
+                  <small>
+                    Already have account?
+                    <i className="fa fa-sign-in-alt"></i>
+                    Please click
+                    <a href="/login"> Login</a>
+                  </small>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </React.Fragment>
   );
 };
